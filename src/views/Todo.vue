3ebbb5f002d246fe53505512419bb856e9add8a5
xiebian <xiebianwhu@gmail.com>
2021-11-11 00:17:21 +0800

    add add/delete task funcs

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,11 +1,85 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo page</h1>
+  <div class="home">
+    <v-text-field
+      class="pa-3"
+      outlined
+      label="Append"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+    ></v-text-field>
+    <v-list v-if="tasks.length" class="pa-0" flat>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon @click.stop="deleteTask(task.id)">
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+    <div v-else class="no-tasks">
+      <v-icon size="100px" color="primary"> mdi-check </v-icon>
+      <div class="text-h5 primary--text">No Tasks</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Todo",
+  data() {
+    return {
+      newTaskTitle: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      let task = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false,
+      };
+      this.tasks.push(task);
+      this.newTaskTitle = "";
+    },
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+  },
 };
 </script>
+
+<style lang="sass">
+  .no-tasks
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%,-50%)
+    opacity: transparent
+</style>
