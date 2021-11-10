20d359c158cb5fcea3e8e9e6eb73d670f4eadf11
xiebian <xiebianwhu@gmail.com>
2021-11-10 15:39:25 +0800

    add drawer

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+      exercitationem commodi adipisci, velit sequi illum voluptates, architecto
+      minima reprehenderit, consectetur eaque facere eius amet quaerat eligendi
+      perferendis doloribus libero pariatur?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+      exercitationem commodi adipisci, velit sequi illum voluptates, architecto
+      minima reprehenderit, consectetur eaque facere eius amet quaerat eligendi
+      perferendis doloribus libero pariatur?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+      exercitationem commodi adipisci, velit sequi illum voluptates, architecto
+      minima reprehenderit, consectetur eaque facere eius amet quaerat eligendi
+      perferendis doloribus libero pariatur?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+      exercitationem commodi adipisci, velit sequi illum voluptates, architecto
+      minima reprehenderit, consectetur eaque facere eius amet quaerat eligendi
+      perferendis doloribus libero pariatur?
+    </p>
   </div>
 </template>
